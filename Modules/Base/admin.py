d7ae21801eb070e2b74f06524fa0e2ddfcb48e26
jzinferno <jzinferno@gmail.com>
2023-06-18 08:41:20 +0300

    Modules: Base: translate Back btn

--- Modules/Base/admin.py
+++ Modules/Base/admin.py
@@ -7,19 +7,22 @@ def admin_text(text):
             'text_1': 'Выберите язык или ввойдите',
             'text_2': 'Выберите язык, сейчас выбран: ',
             'text_3': 'Язык бота',
-            'text_4': 'Настройки'
+            'text_4': 'Настройки',
+            'text_5': 'Назад'
         },
         'uk': {
             'text_1': 'Виберіть мову або увійдіть',
             'text_2': 'Виберіть мову, зараз вибраний: ',
             'text_3': 'Мова бота',
-            'text_4': 'Налаштування'
+            'text_4': 'Налаштування',
+            'text_5': 'Назад'
         },
         'en': {
             'text_1': 'Choose a language or sign in',
             'text_2': 'Select language, currently selected: ',
             'text_3': 'Bot language',
-            'text_4': 'Settings'
+            'text_4': 'Settings',
+            'text_5': 'Back'
         }
     }, text)
 
@@ -29,12 +32,13 @@ def settingsBtns():
         CreateBtn(admin_text('text_4'), 'sign_in')
     )
 
-langsBtns = AddBtns(
-    CreateBtn('Ru', 'ru_lang'),
-    CreateBtn('Uk', 'uk_lang'),
-    CreateBtn('En', 'en_lang'),
-    CreateBtn('Back', 'lang_back_btn'),
-)
+def langsBtns():
+    return AddBtns(
+        CreateBtn('Ru', 'ru_lang'),
+        CreateBtn('Uk', 'uk_lang'),
+        CreateBtn('En', 'en_lang'),
+        CreateBtn(admin_text('text_5'), 'lang_back_btn'),
+    )
 
 @dp.message_handler(commands=['admin'])
 async def main_admin(msg: types.Message):
@@ -44,16 +48,16 @@ async def main_admin(msg: types.Message):
 @dp.callback_query_handler(lambda c: c.data in ['set_lang', 'sign_in', 'ru_lang', 'uk_lang', 'en_lang', 'lang_back_btn'])
 async def process_callback_button(cq: types.CallbackQuery):
     if cq.data == 'set_lang':
-        await EditBtns(cq, text=admin_text('text_2') + GetBotLang(), reply_markup=langsBtns)
+        await EditBtns(cq, text=admin_text('text_2') + GetBotLang(), reply_markup=langsBtns())
     elif cq.data == 'ru_lang':
         SetConfig('lang', 'ru')
-        await EditBtns(cq, text=admin_text('text_2') + GetBotLang(), reply_markup=langsBtns)
+        await EditBtns(cq, text=admin_text('text_2') + GetBotLang(), reply_markup=langsBtns())
     elif cq.data == 'uk_lang':
         SetConfig('lang', 'uk')
-        await EditBtns(cq, text=admin_text('text_2') + GetBotLang(), reply_markup=langsBtns)
+        await EditBtns(cq, text=admin_text('text_2') + GetBotLang(), reply_markup=langsBtns())
     elif cq.data == 'en_lang':
         SetConfig('lang', 'en')
-        await EditBtns(cq, text=admin_text('text_2') + GetBotLang(), reply_markup=langsBtns)
+        await EditBtns(cq, text=admin_text('text_2') + GetBotLang(), reply_markup=langsBtns())
     elif cq.data == 'lang_back_btn':
         await EditBtns(cq, text=admin_text('text_1'), reply_markup=settingsBtns())
     else:
