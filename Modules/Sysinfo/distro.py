0162d1a78c8488c120015cafd758154b3f58c562
jzinferno <jzinferno@gmail.com>
2023-06-28 13:11:25 +0300

    Modules: Sysinfo: Del strip()

--- Modules/Sysinfo/distro.py
+++ Modules/Sysinfo/distro.py
@@ -6,7 +6,7 @@ def sysinfo_distro():
         with open('/etc/os-release') as f:
             for line in f:
                 if line.startswith('NAME='):
-                    distro_name = line.split('=')[1].strip().strip('\"')
+                    distro_name = line.split('=')[1].replace('\"', '')
     elif exists('/product/app') and exists('/system/app') and exists('/vendor/app'):
         distro_name = 'Android ' + subprocess.run(['getprop', 'ro.build.version.release'], capture_output=True).stdout.decode('utf-8')
     else:
