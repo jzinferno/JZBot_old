47cb1275f56cbd3d76267bac1daef340f29c4862
jzinferno <jzinferno@gmail.com>
2023-07-13 10:50:59 +0300

    Modules: Openai: wisper transcribe video_note

--- Modules/OpenAi/openai.py
+++ Modules/OpenAi/openai.py
@@ -14,21 +14,21 @@ def openai_text(number):
             'Вы забыли ввести текст запроса',
             'Не удалось выполнить запрос OpenAi',
             'Нужно ответить на сообщение которое содержит аудиофайл',
-            'Размер аудиофайла не должен превышать 1мб'
+            'Размер аудиофайла не должен превышать 5МБ'
         ],
         'uk': [
             'Ваш запит не може бути більше ніж 1000 символів',
             'Ви забули ввести текст запиту',
             'Не вдалося виконати запит OpenAi',
             'Потрібно відповісти на повідомлення, яке містить аудіофайл',
-            'Розмір аудіофайлу не повинен перевищувати 1мб'
+            'Розмір аудіофайлу не повинен перевищувати 5МБ'
         ],
         'en': [
             'Your request cannot be more than 1000 characters',
             'You forgot to enter the request text',
             'Failed to execute OpenAi request',
             'Reply to a message that contains an audio file',
-            'The size of the audio file must not exceed 1MB'
+            'The size of the audio file must not exceed 5MB'
         ]
     }, number)
 
@@ -96,21 +96,22 @@ async def main_wisper(msg: Message):
             await ReplyMsg(msg, openai_text(3))
         else:
             reply_msg = msg.reply_to_message
-            audio_msg = reply_msg.voice if 'voice' in reply_msg else reply_msg.audio if 'audio' in reply_msg else None
+            audio_msg = reply_msg.voice if 'voice' in reply_msg else reply_msg.audio if 'audio' in reply_msg else reply_msg.video_note if 'video_note' in reply_msg else  None
             if audio_msg is not None:
-                if audio_msg.file_size >= 1048577:
+                if audio_msg.file_size >= 5242880:
                     await ReplyMsg(msg, openai_text(4))
                 else:
                     try:
                         file_id = audio_msg.file_id
-                        file_format = audio_msg.mime_type.split('/')[1]
+                        file_format = 'mp4' if 'video_note' in reply_msg else audio_msg.mime_type.split('/')[1]
                         FileName = f'{outpdir}/{file_id}-{RundomName(10)}'
                         await DownloadFile(file_id, f'{FileName}.{file_format}')
-                        if file_format == 'ogg':
-                            await asyncify(ConvertAudio)(f'{FileName}.{file_format}', f'{FileName}.wav', 'wav')
-                            file_format = 'wav'
-                        transcript = await openai.Audio.atranscribe('whisper-1', open(f'{FileName}.{file_format}', 'rb'))
-                        await ReplyMsg(msg, transcript.text.strip())
+                        if file_format in ['mp4', 'ogg']:
+                            await asyncify(ConvertAudio)(f'{FileName}.{file_format}', f'{FileName}.mp3', 'mp3')
+                            file_format = 'mp3'
+                        with open(f'{FileName}.{file_format}', 'rb') as audio_file:
+                            transcribe = await openai.Audio.atranscribe('whisper-1', audio_file)
+                        await ReplyMsg(msg, transcribe.text.strip())
                     except:
                         await ReplyMsg(msg, openai_text(2))
             else:
