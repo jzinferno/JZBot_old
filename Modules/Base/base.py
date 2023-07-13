55bd7998ebfebbbff7fef5b00fe40d1a21d3746b
jzinferno <jzinferno@gmail.com>
2023-07-13 22:56:32 +0300

    JZBot: Add /help command (ru)

--- Modules/Base/base.py
+++ Modules/Base/base.py
@@ -1,12 +1,29 @@
-from JZBot import dp, version, GetChatStatus, ReplyMsg, Message
+from JZBot import dp, version, GetChatStatus, GetBotLang, ReplyMsg, Message
+from Modules import GetModulesHelp
+
+@dp.message_handler(commands=['help', 'start'])
+async def main_version(msg: Message):
+    JZBotHelpFull = GetModulesHelp()
+    if len(msg.text.split()[1:]) >= 1:
+        ModuleName = msg.text.split()[1]
+        if ModuleName in JZBotHelpFull:
+            await ReplyMsg(msg, JZBotHelpFull[ModuleName]['help'])
+        else:
+            await ReplyMsg(msg, 'No module: ' + ModuleName)
+    else:
+        JZBotHelp = ['https://github.com/jzinferno/JZBot\n', '/help <module name>\n', 'Installed Modules:']
+        for ModuleName in JZBotHelpFull:
+            JZBotHelp.append(ModuleName + ' - ' + JZBotHelpFull[ModuleName]['author'])
+        await ReplyMsg(msg, '\n'.join(JZBotHelp))
 
 @dp.message_handler(commands=['id'])
 async def main_id(msg: Message):
     await ReplyMsg(msg, msg.chat.id)
 
-@dp.message_handler(commands=['myid'])
-async def main_myid(msg: Message):
-    await ReplyMsg(msg, msg.from_user.id)
+@dp.message_handler(commands=['userid'])
+async def main_userid(msg: Message):
+    id = msg.reply_to_message.from_user.id if 'reply_to_message' in msg else msg.from_user.id
+    await ReplyMsg(msg, id)
 
 @dp.message_handler(commands=['version'])
 async def main_version(msg: Message):
--- Modules/__init__.py
+++ Modules/__init__.py
@@ -1,6 +1,25 @@
-from JZBot import workdir
-from glob import glob
-from os import path
+from JZBot import workdir, GetBotLang
+from os import path, listdir
+import json
 
-for ModuleDir in [d for d in glob(workdir + '/Modules/*') if path.isfile(path.join(d, '__init__.py'))]:
-    exec('from Modules.{} import dp'.format(ModuleDir.split('/')[-1]))
+modulesdir = workdir + '/Modules'
+
+def GetModules(full_path=False):
+    ModulesArr = []
+    for folder in listdir(modulesdir):
+        folder_path = path.join(modulesdir, folder)
+        if path.isdir(folder_path) and '__init__.py' in listdir(folder_path):
+            ModulesArr.append(folder_path if full_path != False else folder)
+    ModulesArr.sort()
+    return ModulesArr
+
+def GetModulesHelp():
+    HelpArr = {}
+    for ModuleDir in GetModules(full_path=True):
+        with open(ModuleDir + '/module.json', 'r+') as jfile:
+            data = json.load(jfile)
+        HelpArr[ModuleDir.split('/')[-1]] = {'author': data['author'], 'help': data['help'][GetBotLang()]}
+    return HelpArr
+
+for ModuleDir in GetModules():
+    exec('from Modules.{} import dp'.format(ModuleDir))
